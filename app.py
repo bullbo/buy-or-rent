a4ebbdba6e62401442318098bab9c1034c980dd9
bullbo <amin.bullarif@gmail.com>
2020-12-12 17:42:16 +0100

    app v1

--- app.py
+++ app.py
@@ -4,12 +4,9 @@
 # visit http://127.0.0.1:8050/ in your web browser.
 
 import dash
-import dash_daq as daq
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
-import plotly.express as px
-import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
 
